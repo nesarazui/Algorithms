04d37afa069e2dc6331f22419f787afb69c2e007
Nesara Kishor <nesara.zui@gmail.com>
2020-05-12 15:18:20 -0400

    branch-sums-bst

---
+++ branch-sums.js
@@ -0,0 +1,31 @@
+// Input: Binary Tree; Output: List of tree's branch sums ordered from leftmost branch to rightmost branch sum
+
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function branchSums (root) {
+    const sums = []
+    calculateBranchSums(root, 0, sums) 
+    return sums
+}
+
+
+function calculateBranchSums (node, runningSum, sums) {
+    if (!node) {
+        return 
+    }
+
+    const newRunningSum = runningSum + node.value
+    if (!node.left && !node.right) {
+        sums.push(newRunningSum)
+        return
+    }
+    calculateBranchSums(node.left, newRunningSum, sums)
+    calculateBranchSums(node.right, newRunningSum, sums)
+}
