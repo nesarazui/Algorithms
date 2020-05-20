109cf4a8a9b0cefa2f3e4dd15ec6721ed66f36c7
Nesara Kishor <nesara.zui@gmail.com>
2020-05-20 16:09:53 -0400

    fib

---
+++ fibonacci.js
@@ -0,0 +1,12 @@
+
+//function that takes an integer 'n' and returns the nth Fibonacci number
+
+function fibonacci (n) {
+    if (n === 2) {
+        return 1
+    } else if (n === 1) {
+        return 0
+    } else {
+        return fibonacci(n-1) + fibonacci(n - 2)
+    }
+}
